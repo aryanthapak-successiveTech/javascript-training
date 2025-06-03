c744720b2dc0021361739d3a32b0cdf1e3259698
aryan.thapak@successive.tech <aryan.thapak@aryanthapakss-416.successivetech.com>
2025-06-03 16:24:22 +0530

    feat-added html generation

--- Question2.js
+++ Question2.js
@@ -9,4 +9,17 @@ function showDetails(){
     console.log("Phone Number : ",phone_no);
 }
 
+let body=document.getElementsByTagName("body")[0];
+let nameHeader=document.createElement("h1");
+nameHeader.textContent=`Name : ${userName}`;
+body.appendChild(nameHeader);
+
+let ageHeader=document.createElement("h1");
+ageHeader.textContent=`Age : ${age}`;
+body.appendChild(ageHeader);
+
+let phoneHeader=document.createElement("h1");
+phoneHeader.textContent=`Phone Number : ${phone_no}`;
+body.appendChild(phoneHeader);
+
 showDetails();
