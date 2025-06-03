2d8299f5dc692241f5e15bbdd51d1ebd4310eb1f
aryan.thapak@successive.tech <aryan.thapak@aryanthapakss-416.successivetech.com>
2025-06-03 13:23:43 +0530

    Question-1 Fix

--- Question1.js
+++ Question1.js
@@ -1,7 +1,13 @@
 //Function Based Approach
-
+let prevAns=undefined
 function rand(){
-    return Math.floor(Math.random() * 10) + 1;
+    let newAns=undefined;
+    do{
+        newAns= Math.floor(Math.random() * 10) + 1;
+    }while(prevAns===newAns)
+        prevAns=newAns;
+    return newAns;
 }
 
+console.log(rand());
 console.log(rand());
--- Question6.js
+++ Question6.js
@@ -1,11 +1,11 @@
 let num=parseInt(prompt("Enter a number"));
 
 function checkEven(num){
-    return num%2==0?"True":"False";
+    return num%2===0?"True":"False";
 }
 
 console.log(checkEven(num));
 
-const isEven=(num)=>num%2==0?"True":"False";
+const isEven=(num)=>num%2===0?"True":"False";
 
 console.log(isEven(num));
