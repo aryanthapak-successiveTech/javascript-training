4bbfff27b8b7a9244a0b4636380e4a68d3cb0d7d
aryan.thapak@successive.tech <aryan.thapak@aryanthapakss-416.successivetech.com>
2025-06-04 15:46:23 +0530

    feat-added input validations in each question with their question description

--- Question4.js
+++ Question4.js
@@ -1,7 +1,12 @@
+// Write a function expression that takes in a number and returns its square.
+
 // Function Approach
 let num=prompt("Enter a number")
 
 const square=function (num){
+    if(isNaN(num)){
+        console.error("enter a number")
+    }
     return num*num;
 }
 
@@ -9,6 +14,11 @@ console.log(square(num));
 
 //Arrow Function Approach
 
-const findSquare=(num)=>num*num;
+const findSquare=(num)=>{
+    if(isNaN(num)){
+        console.error("enter a number")
+    }
+    return num*num;
+};
 
 console.log(findSquare(num));
--- Question5.js
+++ Question5.js
@@ -1,7 +1,13 @@
+//5. Write a function expression that takes in two numbers and returns their sum.
+
 //Function Approach
 let num1=parseInt(prompt("Enter a number"));
 let num2=parseInt(prompt("Enter a number"));
+
 const add=function (num1,num2){
+    if(isNaN(num1) ||isNaN(num2)){
+        console.error("enter a number")
+    }
     return num1+num2;
 }
 
@@ -9,6 +15,11 @@ console.log(add(num1,num2))
 
 //Arrow Function Approach
 
-const sum=(num1,num2)=>num1+num2;
+const sum=(num1,num2)=>{
+    if(isNaN(num1) ||isNaN(num2)){
+        console.error("enter a number");
+    }
+    return num1+num2;
+};
 
 console.log(sum(num1,num2));
