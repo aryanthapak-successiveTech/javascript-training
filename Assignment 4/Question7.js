a6e456af65aca110784f402f1fa17f666b948681
aryan.thapak@successive.tech <aryan.thapak@aryanthapakss-416.successivetech.com>
2025-06-05 18:28:26 +0530

    refact: added duplicates indices storing feature and fixed question-13

--- Assignment 4/Question7.js
+++ Assignment 4/Question7.js
@@ -5,26 +5,29 @@ const { takeInput, closeInput } = require("./Input");
 const duplicateElement = async () => {
   let string = await takeInput("Enter a set of numbers seprated by commas : ");
   closeInput();
-  const arr=string.split(',');
-  const elementCountMap=new Map();
-  const visited=new Set();
-
-  for(let i=0;i<arr.length;i++){
+  const arr = string.split(",");
+  const elementIndicesMap = new Map();
+  for (let i = 0; i < arr.length; i++) {
     if (isNaN(arr[i])) {
       console.log("Expected a number");
       return;
     }
-    let ele=arr[i];
-    elementCountMap.set(ele,(elementCountMap.has(ele)?elementCountMap.get(ele):0)+1);
-}
-
-for(let i=0;i<arr.length;i++){
-    if(elementCountMap.get(arr[i])>1 && !visited.has(arr[i])){
-        console.log(i);
-        visited.add(arr[i]);
+    let ele = arr[i];
+    if (!elementIndicesMap.has(ele)) {
+      elementIndicesMap.set(ele, []);
     }
-}
+    elementIndicesMap.get(ele).push(i);
+  }
 
-}
+  for (const [ele, indices] of elementIndicesMap.entries()) {
+    if (indices.length > 1) {
+      let res = `${ele} is present on`;
+      indices.forEach((element) => {
+        res += " " + element + ",";
+      });
+      console.log(res);
+    }
+  }
+};
 
-duplicateElement()
+duplicateElement();
