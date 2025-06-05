45e3d5cf6c6f354b7a17b74b8f5bf07492c9c609
aryan.thapak@successive.tech <aryan.thapak@aryanthapakss-416.successivetech.com>
2025-06-05 18:28:26 +0530

    refact: added duplicates indices storing feature and fixed question-13

--- Assignment 4/Question13.js
+++ Assignment 4/Question13.js
@@ -7,16 +7,12 @@ const inverseOfArray = async () => {
   let string1 = await takeInput("Enter a set of numbers seprated by commas : ");
   closeInput();
   const arr=string1.split(",");
-  let i=0,j=arr.length-1;
-  while(i<j){
-     let temp=arr[i];
-     arr[i]=arr[j];
-     arr[j]=temp;
-     i++;
-     j--;
+  const newArr=[];
+  for(let i=0;i<arr.length;i++){
+    newArr[arr[i]]=i;
   }
 
-  console.log(arr);
+  console.log(newArr);
 }
 
 inverseOfArray();
